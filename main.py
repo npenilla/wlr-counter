428175fd9c66ad8aa83c0f555723da1b24786c00
Nigel Penilla <npenilla23@ufl.edu>
2020-09-07 22:38:25 -0400

    Finished automatic tweeting capabilities with linux crontab

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
 import config, tweepy, time, datetime, math
 from datetime import datetime
 from dateutil import relativedelta
-from time import sleep 
+from time import sleep
 
 auth = tweepy.OAuthHandler(config.consumer_key, config.consumer_secret)
 auth.set_access_token(config.access_token, config.token_secret)
 api = tweepy.API(auth)
 
+curr_date = datetime.today()
 start_date = datetime(2018, 8, 18)
 
 diff = relativedelta.relativedelta(curr_date, start_date)
-
 def build_string(years, months, days):
-    msg = "Whole Lotta Red has NOT been released.\n"
+    msg = "Whole Lotta Red has NOT been released.\n\n"
     msg += "It's been " + str(years) + " years"
     if(months == 0):
         if(days != 0):
@@ -27,11 +27,9 @@ def build_string(years, months, days):
 
     msg += " since the project has been announced."
     return msg
-
 years = diff.years
 months = diff.months
 days = diff.days
 msg = build_string(years, months, days)
 api.update_status(msg)
-
 
